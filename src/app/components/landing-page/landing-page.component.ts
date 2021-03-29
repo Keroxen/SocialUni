63dd85edd9961622361501814a7257381dc7f090
Alin <alin.ald99@gmail.com>
2021-03-29 23:52:43 +0300

    landing page redesign + moved login and signup to dialog

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { AuthResponseData } from '../../shared/auth.model';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { AuthDialogComponent } from '../auth-dialog/auth-dialog.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
     selector: 'app-landing-page',
@@ -11,50 +10,18 @@ import { Router } from '@angular/router';
     styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-    isLoginMode = false;
-    error = null;
 
-    constructor(private authService: AuthService, private router: Router) {
+    constructor(private router: Router, public dialog: MatDialog, private authService: AuthService) {
     }
-
-    authForm = new FormGroup({
-        firstName: new FormControl(''),
-        lastName: new FormControl(''),
-        email: new FormControl(''),
-        password: new FormControl(''),
-    });
 
     ngOnInit(): void {
-        setTimeout(() => {
-            this.authService.logout();
-        }, 10000)
     }
 
-    onSubmit() {
-        console.log(this.authForm.value);
-        if (!this.authForm.valid) {
-            return;
-        }
-
-        const email = this.authForm.value.email;
-        const password = this.authForm.value.password;
-        console.log(this.authForm.valid)
-        let authObs: Observable<AuthResponseData>;
-
-        // if (this.isLoginMode) {
-            authObs = this.authService.login(email, password);
-        // } else {
-        //     authObs = this.authService.signup(email, password);
-        // }
-
-        authObs.subscribe(resData => {
-            console.log(resData);
-            this.router.navigate(['/']);
-        }, errorMessage => {
-            console.log(errorMessage);
-            this.error = errorMessage;
+    openDialog(authMode: string) {
+        const dialogRef = this.dialog.open(AuthDialogComponent);
+        this.authService.updateAuthMode(authMode);
+        dialogRef.afterClosed().subscribe(result => {
+            // console.log(result);
         });
-
-        this.authForm.reset();
     }
 }
