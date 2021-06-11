1f6a85b21f00d94536a74d90ab60d5ea8ffc2c71
Alin <alin.ald99@gmail.com>
2021-06-11 16:35:50 +0300

    save comments in Firestore

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable, Subject } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
+import firebase from 'firebase';
+import { AngularFireAuth } from '@angular/fire/auth';
+
 import { DataService } from '@services/data.service';
 import { Post } from '@models/post.model';
-import { Observable, Subject } from 'rxjs';
-import { take } from 'rxjs/operators';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Comment } from '@models/comment.model';
+import { AuthService } from '@services/auth.service';
 
 @Component({
     selector: 'app-home',
@@ -11,6 +17,7 @@ import { FormControl, FormGroup } from '@angular/forms';
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, OnDestroy {
+    currentUid: string | undefined;
 
     latestPosts$: Observable<Post[]> | any;
 
@@ -22,7 +29,21 @@ export class HomeComponent implements OnInit, OnDestroy {
         newComment: new FormControl('')
     });
 
-    constructor(private dataService: DataService) {
+    userFirstName: string | undefined;
+    userLastName: string | undefined;
+    userImageURL: string | undefined;
+
+    constructor(private dataService: DataService, private afs: AngularFirestore, private authService: AuthService,
+                private afAuth: AngularFireAuth) {
+        this.afAuth.authState.pipe(takeUntil(this.destroy$)).subscribe(user => {
+            this.currentUid = user?.uid;
+            this.dataService.getUserData(this.currentUid).ref.get().then(doc => {
+                console.log(doc.data());
+                this.userFirstName = doc.data()?.firstName;
+                this.userLastName = doc.data()?.lastName;
+                this.userImageURL = doc.data()?.imageURL;
+            });
+        });
     }
 
     ngOnInit(): void {
@@ -41,4 +62,21 @@ export class HomeComponent implements OnInit, OnDestroy {
         post.areCommentsVisible = !post.areCommentsVisible;
     }
 
+    onSubmitComment(postId: string): void {
+        console.log(this.newCommentForm.get('newComment')?.value);
+        console.log(postId);
+        const newComment = this.newCommentForm.get('newComment')?.value;
+        if (newComment && newComment.trim()) {
+            this.afs.collection<Post>('posts').doc(postId).collection<Comment>('comments').add({
+                content: newComment,
+                created: firebase.firestore.FieldValue.serverTimestamp(),
+                userFirstName: this.userFirstName,
+                userLastName: this.userLastName,
+                userImageURL: this.userImageURL,
+            });
+            this.newCommentForm.reset();
+        } else {
+            console.log('empty post');
+        }
+    }
 }
---
+++ src/app/models/comment.model.ts
@@ -0,0 +1,9 @@
+export interface Comment {
+    content: string;
+    created: any;
+    userFirstName: string | undefined;
+    userLastName: string | undefined;
+    userImageURL: string | undefined;
+    // numberOfLikes: number;
+    // numberOfDislikes: number;
+}
